0a6523e7e7ae8c2f4895fba9a6649a27950b30f6
Kabir079 <69202422+Kabir079@users.noreply.github.com>
2020-08-05 09:29:52 +0530

    Wrote comments.
    So , this is code which asks the user to enter a number and then it gives the entered number table till 20’s .

--- 1st LEVEL.py
+++ 1st LEVEL.py
@@ -1,7 +1,7 @@
 def a():
-    a=int(input("Enter the number inorder to get it's table : "))
+    a=int(input("Enter the number inorder to get it's table : ")) #This line is input line
     b=1
-    while(b<=20):
-        print(a,"X",b,"=",a*b)
+    while(b<=20): #This loop will go upto till 20 , so the table for the entered number will be shown till it's 20's
+        print(a,"X",b,"=",a*b) 
         b=b+1
     
